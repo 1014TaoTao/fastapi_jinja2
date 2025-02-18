fc38d45ed2d7c69abdc9092db864a6b822a4af8c
zhangtao <9480807882@qq.com>
2025-02-18 18:11:20 +0800

    代码移除调试部分

--- app/core/router.py
+++ app/core/router.py
@@ -2,11 +2,9 @@
 
 from fastapi import APIRouter, FastAPI
 
-from app.view.views import router
 from app.view.demo import router as demo_router
 
 def register_router_handler(app: FastAPI):
     Router = APIRouter(prefix="")
-    Router.include_router(router=router, tags=["项目接口"])
     Router.include_router(router=demo_router, tags=["案例接口"])
     app.include_router(Router)
